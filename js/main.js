afa2e8781eed9dfb77b3aef679d1414e7a48ac6b
saravogl <saravogl11@gmail.com>
2016-01-30 16:46:55 -0600

    Set-up of HTML, CSS, and JS for Final Project
    Completed initial HTML markup, set up style.css and normalize.css, as
    well as main.js with pseudo code.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Page will open to h1 and nav cenetered on page with full-stretch background image
+
+// Two options for page navigation: 
+
+// Option 1: User clicks on nav links...
+
+// When user clicks on a nav link, page will scroll to the appropriate section.
+// Nav bar will be fixed to the top of the page permanently as the user scrolls.
+
+// Option 2: User scrolls...
+
+// When user scrolls, nav bar becomes permanently fixed to the top of the page
+
+// Notes: I'm in the process of figuring out the styling of my page, which makes
+// it difficult to think through other js and jquery methods at this time. By next 
+// week/end, I plan to have fleshed out my content a bit more (right now, it's mostly
+// song lyrics) and have a better idea for the styling of my page. But I think taking 
+// some time this week to research and look around the web will help me find some ideas 
+// and challenges to take on for my css and js. 
