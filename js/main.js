5781c47bcbb83747e2e6afef3f08be742e467761
saravogl <saravogl11@gmail.com>
2016-02-17 22:37:36 -0600

    Committing Changes for Final Project
    Mobile-Navigation css and js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// Scroll to top of page @ refresh //
+
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+
 // Back to top button //
 
 var amountScrolled = 300;
@@ -18,7 +24,6 @@ $('a.back-to-top').on('click', function(e) {
 	return false;
 });
 
-
 // Scroll to section //
 
 $('.menu a').on('click', function(e) {
@@ -29,9 +34,29 @@ $('.menu a').on('click', function(e) {
     return false;
 }); 
 
-// 'Contact me' to Say Hello Section //
+// Lifework: 'Say Hello' to say hello section //
 
-$('.lets-dance a:last-of-type').on('click', function(e) {
+$('.life-work .hello').on('click', function(e) {
+    e.preventDefault();
+    var href = $(this).attr('href');
+    var anchor = $(href).offset();
+    $('body').animate({ scrollTop: anchor.top }, 700);
+    return false;
+}); 
+
+// Lifework: 'Come join me at a class' to let's dance section //
+
+$('.life-work .dance').on('click', function(e) {
+    e.preventDefault();
+    var href = $(this).attr('href');
+    var anchor = $(href).offset();
+    $('body').animate({ scrollTop: anchor.top }, 700);
+    return false;
+}); 
+
+// Let's Dance: 'Contact me' to Say Hello Section //
+
+$('.lets-dance .hello').on('click', function(e) {
 	e.preventDefault();
     var href = $(this).attr('href');
     var anchor = $(href).offset();
@@ -41,12 +66,33 @@ $('.lets-dance a:last-of-type').on('click', function(e) {
 
 // Fixed Scroll Menu //
 
-var num = 300; //number of pixels before modifying styles
+var navTop = $('.menu').offset().top;
 
 $(window).bind('scroll', function () {
-    if ($(window).scrollTop() > num) {
+    if ($(window).scrollTop() > navTop) {
         $('.menu').addClass('fixed');
     } else {
         $('.menu').removeClass('fixed');
     }
+});
+
+// Mobile Nav //
+
+$('.hamburger').on('click', function () {
+    if ($('.mobile-nav').hasClass('open')) {
+        console.log($('.mobile-nav').width());
+        $('.mobile-nav').animate({
+            right: -200
+        }, function() {
+            $(this).removeClass('open');
+        });
+    } else {
+        $('.mobile-nav').animate({
+            right: 0
+        }, function() {
+            $(this).addClass('open');
+        });
+    }
+
+    
 });
